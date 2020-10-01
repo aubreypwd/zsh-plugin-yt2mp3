b11de442dca655029ba54a1cde40b3d904df05e3
Aubrey Portwood <aubreypwd@users.noreply.github.com>
2020-10-01 13:38:40 -0600

    Require youtube-dl from Homebrew

--- zsh-plugin-yt2mp3.plugin.zsh
+++ zsh-plugin-yt2mp3.plugin.zsh
@@ -1,8 +1,14 @@
 #!/bin/zsh
 
-###
- # Download a Youtube Video as mp3 via URL
- #
- # @since 1.0.0
- ##
-alias yt2mp3="youtube-dl --extract-audio --audio-format mp3"
+if ! [[ -x $(command -v youtube-dl) ]]; then
+	echo "Please install youtube-dl:";
+	echo "\tHomebrew: brew install youtube-dl"
+else
+
+	###
+	 # Download a Youtube Video as mp3 via URL
+	 #
+	 # @since 1.0.0
+	 ##
+	 alias yt2mp3="youtube-dl --extract-audio --audio-format mp3"
+	fi
