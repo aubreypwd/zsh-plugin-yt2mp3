250af6ce05ec57104fcc293a8b80fa33e97cee2e
Aubrey Portwood <aubreypwd@users.noreply.github.com>
2024-06-20 20:00:18 -0600

    Use yt-dlp

--- zsh-plugin-yt2mp3.plugin.zsh
+++ zsh-plugin-yt2mp3.plugin.zsh
@@ -1,12 +1,12 @@
 #!/bin/zsh
 
-if [[ -x $(command -v brew) ]] && [[ ! -x $(command -v youtube-dl) ]]; then
-	brew reinstall youtube-dl
+if [[ -x $(command -v brew) ]] && [[ ! -x $(command -v yt-dlp) ]]; then
+	brew reinstall yt-dlp
 fi
 
-if ! [[ -x $(command -v youtube-dl) ]]; then
-	echo "Please install youtube-dl:";
-	echo "\tHomebrew: brew install youtube-dl"
+if ! [[ -x $(command -v yt-dlp) ]]; then
+	echo "Please install yt-dlp:";
+	echo "\tHomebrew: brew install yt-dlp"
 else
 
 	###
@@ -14,5 +14,5 @@ else
 	 #
 	 # @since 1.0.0
 	 ##
-	 alias yt2mp3="youtube-dl --extract-audio --audio-format mp3"
+	 alias yt2mp3="yt-dlp --extract-audio --audio-format mp3"
 fi
